75258e640d23c39003849c91b99439fa806da105
Andres Hortua <fandresh7@gmail.com>
2024-02-09 12:29:40 -0500

    fix search modal functionallity

--- src/app/courses/components/search-modal/search-modal.component.ts
+++ src/app/courses/components/search-modal/search-modal.component.ts
@@ -1,40 +1,56 @@
-import { AsyncPipe } from '@angular/common'
+import { AsyncPipe, JsonPipe } from '@angular/common'
 import { Component, OnInit, inject } from '@angular/core'
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { ScrollingModule } from '@angular/cdk/scrolling'
 
-import { Observable, debounceTime, tap } from 'rxjs'
+import { Observable, combineLatest, debounceTime, map, switchMap } from 'rxjs'
 
 import { Lesson } from '@courses/logic/models/lessons'
 import { LessonCardComponent } from '../lesson-card/lesson-card.component'
 import { LevelsStore } from '@courses/store/levels.store'
+import { Glossary } from '@courses/logic/models/glossary'
+import { GlossaryService } from '@courses/services/glossary.service'
+import { GlossaryCardComponent } from '../glossary-card/glossary-card.component'
 
 @Component({
   selector: 'app-search-modal',
   standalone: true,
-  imports: [ReactiveFormsModule, LessonCardComponent, AsyncPipe],
+  imports: [
+    ReactiveFormsModule,
+    LessonCardComponent,
+    AsyncPipe,
+    JsonPipe,
+    GlossaryCardComponent,
+    ScrollingModule
+  ],
   templateUrl: './search-modal.component.html',
   styleUrl: './search-modal.component.css'
 })
 export class SearchModalComponent implements OnInit {
   fb = inject(FormBuilder)
   levelsStore = inject(LevelsStore)
+  glossaryService = inject(GlossaryService)
 
-  lessons$!: Observable<Lesson[]>
+  data$!: Observable<{ lessons: Lesson[]; glossary: Glossary[] }>
 
   form: FormGroup = this.fb.group({
     search: ''
   })
 
   ngOnInit() {
-    this.form.valueChanges
-      .pipe(
-        debounceTime(500),
-        tap(({ search }) => {
-          if (!search) return
-          this.lessons$ = this.levelsStore.getSearchLessons(search)
-        })
-      )
-      .subscribe()
+    this.data$ = this.form.valueChanges.pipe(
+      debounceTime(500),
+      switchMap(({ search }) => {
+        const lessons$ = this.levelsStore.getSearchLessons(search)
+        const glossary$ = this.glossaryService.searchByQuery(search)
+
+        return combineLatest([lessons$, glossary$]).pipe(
+          map(([lessons, glossary]) => {
+            return { lessons, glossary }
+          })
+        )
+      })
+    )
   }
 
   checkSearchValue() {
@@ -44,7 +60,5 @@ export class SearchModalComponent implements OnInit {
 
   reset() {
     this.form.reset()
-
-    this.lessons$ = this.levelsStore.getSearchLessons('')
   }
 }
